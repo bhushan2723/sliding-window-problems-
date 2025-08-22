e3c79882c2d381bdd5cc291102072164b8a1e9f9
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-08-22 22:33:29 +0530

    Create L:argestPerimeter.java

---
+++ L:argestPerimeter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        
+        for (int i = n - 1; i >= 2; i--) {
+            int a = nums[i];
+            int b = nums[i - 1];
+            int c = nums[i - 2];
+
+          
+            if (b + c > a  && a + b > c && b + c > a) {
+                return a + b + c;
+            }
+        }
+
+        return 0;
+    }
+}
