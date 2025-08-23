d0239ec1500c438968f2edfb66edb0aca16c8a8a
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-08-23 20:17:08 +0530

    Create isToeplitzMatrix.java

---
+++ isToeplitzMatrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+
+        // check from (1,1) → (m-1,n-1)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i-1][j-1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
